e12f423e42a636a416771a24b505381bdc1aba45
evan11 <mill3131@purdue.edu>
2024-03-30 15:51:57 -0400

    I added everything

--- tester.py
+++ tester.py
@@ -1,4 +1,8 @@
-import functions
+import insert
 
-id = insert.insert("evan", "image.jpg")
-insert.download("evan_" + str(id) + ".jpg", "downloaded.jpg")
+id = insert.insert("evan", "image.jpg", "green cloak")
+id = insert.insert("evan", "receipt.jpg", "not clothes")
+id = insert.insert("evan", "vscode.png", "screenshot of code")
+data = insert.download_all("evan")
+for item in data:
+    print(item)
