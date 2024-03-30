572f1ecc4103c5a78ebb22fb0e41912be9b1a60d
evan11 <mill3131@purdue.edu>
2024-03-30 14:23:05 -0400

    New stuff

--- supa.py
+++ supa.py
@@ -7,10 +7,13 @@ url= "https://rzdyvqcuzbdcaibdrypw.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6ZHl2cWN1emJkY2FpYmRyeXB3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MTQ1MzksImV4cCI6MjAyNzM5MDUzOX0.pqHVSybnPnXseAdDJ3bhTWUsmq7n3j-iGFRt_R4RnQQ"
 supabase: Client = create_client(url, key)
 
-response = supabase.table('evan').select("*").execute()
-print(response)
-data = response('data')
+response = supabase.table('evan').select("id", "description").execute()
+
+print(response.data)
+data = response.data
 for item in data:
     print(item)
+
+response = supabase.table('evan').select("id", "description").execute()
 
     # https://supabase.com/docs/reference/python/insert
