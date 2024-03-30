e3f63afc808df17b5a3b276dc476a8191f0c4e16
argha-saha <saha56@purdue.edu>
2024-03-30 14:22:48 -0400

    weather api connection

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ from flask import Flask, request, redirect, url_for
 #from postgrest_py import PostgrestClient
 from dotenv import load_dotenv
 import os
+import requests
+from pprint import pprint as pp
 
 from supabase import create_client, Client
 
+load_dotenv()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client('https://yhjgqrrmkmbkiunfwxbo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloamdxcnJta21ia2l1bmZ3eGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MTQzODIsImV4cCI6MjAyNzM5MDM4Mn0.BLD1z5AuIYV77SKF8t0z5gJIHJ88MyFAkBvo4Kc3PO0')
+supabase: Client = create_client(url, key)
 
-load_dotenv()
 app = Flask(__name__)
 
-@app.route("/")
-def test():
 
+@app.route("/auth")
+def test():
     choice = 0
 
     while choice != '-1':
-
         choice = input("Sign Up, Sign In, Sign Out, View Data (1, 2, 3, 4): ")
 
         if choice == "1":
@@ -44,18 +45,58 @@ def test():
         elif choice == '4':
             data = supabase.auth.get_user()
             print(data.user.id)
-    
-    
-    return "Hello"
 
 
 def forward_string():
-    llm_prompt = input("Prompt: ")
-    return llm_prompt
+    return "Hello"
 
 # Connect to weather API
+@app.route('/weather')
+def get_weather():
+    zipcode = input("Enter Zipcode: ")
+    api_key = "882d7c4617b36d2101b88c388111c3a0"
+    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid={api_key}&units=imperial"
+    print("Hello")
+    response = requests.get(url)
+    weather_data = response.json()
 
-# Call ollama
+    if response.status_code == 200:
+        temperature = weather_data["main"]["temp"]
+        humidity = weather_data["main"]["humidity"]
+        description = weather_data["weather"][0]["description"]
+
+        print(f"Temperature: {temperature}°C\n Humidity: {humidity}%\n Description: {description}")
+        prompt = f"What should I wear today? This is the weather in my city: Temperature: {temperature}°C\n Humidity: {humidity}%\n Description: {description}"
+        messages = []
+        messages.append({"roles": "user", "content": prompt})
+        return generate_response(requests.Session(), messages)
+    else:
+        print("Error fetching weather data")
+        #exit()
+
+
+    # Call ollama
+def generate_response(session, message):
+    url_llama = "http://localhost:11434/api/chat"
+
+    payload = {
+        "model": "llama2-uncensored",
+        'messages': message,
+        "stream": False,
+    }
+
+    # Send POST request using the session
+    response = session.post(url_llama, json=payload)
+
+    # Check response
+    if response.status_code == 200:
+        result = response.json()["message"]["content"]
+
+        pp(result)
+    else:
+        print("Request failed with status code:", response.status_code)
+        print("Response:")
+        print(response.text)  # Print error response
 
 # Get preferences (outfit type)
 def get_preferences():
