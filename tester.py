1c9abc310a5aed1ec26c0567970bc595c3a5c7c3
evan11 <mill3131@purdue.edu>
2024-03-30 16:14:22 -0400

    Kanyes #2 fan

--- tester.py
+++ tester.py
@@ -1,8 +1,8 @@
 import insert
 
 id = insert.insert("evan", "image.jpg", "green cloak")
-id = insert.insert("evan", "receipt.jpg", "not clothes")
-id = insert.insert("evan", "vscode.png", "screenshot of code")
+id = insert.insert("evan", "Images/acne2.png", "white shirt")
+id = insert.insert("evan", "Images/clothingimage1.png", "blue sweater")
 data = insert.download_all("evan")
 for item in data:
     print(item)
